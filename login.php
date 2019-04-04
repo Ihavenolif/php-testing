f39a672f9a9b6dbaed1cee2d385514d7359cc910
TheWallovec <42898548+TheWallovec@users.noreply.github.com>
2019-04-04 15:38:29 +0200

    mysql database p1

--- login.php
+++ login.php
@@ -5,6 +5,17 @@
     $username = $_REQUEST["username"];
     $password = $_REQUEST["password"];
 
+    //Create connection to MySQL Database
+    $connection = new mysqli("localhost", "id9130300_admin", "admin");
+
+    //Check connection
+    if($connection->connect_error)
+    {
+        exit("Connection failed: " . $connection->connect_error);
+    } else {
+        echo("Connected successfully!");
+    }
+
     if(!file_exists($username . ".txt"))
     {
         $profile = fopen($username . ".txt", "x");
@@ -12,7 +23,9 @@
         fwrite($profile, "username=" . $username . "\n" . "password=" . $password);
         fclose($profile);
     } else {
-        echo '<script> alert("USer already exists!")</script>'
+        echo '<script> 
+                alert("User already exists!");
+              </script>';
     }
 ?>
 
