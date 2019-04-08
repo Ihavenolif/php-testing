84dedd3e1109b429055cc499e80e006f14d029d1
TheWallovec <42898548+TheWallovec@users.noreply.github.com>
2019-04-08 19:41:25 +0200

    cookieclicker a0.2

--- cookieclicker/cookieclicker.js
+++ cookieclicker/cookieclicker.js
@@ -1,3 +1,6 @@
+var cps = 0, cpc = 1, cursorPrice = 10;
+var cookies = document.getElementById("cookieValueDisplay").nodeValue;
+
 function gotoLogin()
 {
     window.location = "../login.html";
@@ -7,3 +10,31 @@ function mainPage()
 {
     window.location = "../index.html";
 }
+
+function increment(x)
+{
+    x += 1;
+}
+
+document.getElementById("newCursor").addEventListener("click", function(){
+    if (cookies >= cursorPrice){
+        cps += 1;
+        cookies -= cursorPrice;
+        cursorPrice *= 1.5;
+        document.getElementById("cursorBuyPriceValue").innerHTML = Math.round(cursorPrice);
+    } else {
+        alert("Not enough cookies!");
+    }
+})
+
+document.getElementById("cookieImage").addEventListener("click", function(){
+    cookies +=cpc;
+    document.getElementById("cookieValueDisplay").innerHTML = "Current cookies: " + Math.round(cookies);
+});
+
+setInterval(function()
+{
+    cookies += (cps/10);
+    document.getElementById("cookieValueDisplay").innerHTML = "Current cookies: " + Math.round(cookies);
+    document.getElementById("cps").innerHTML = "Cookies per second: " + cps;
+}, 100);
