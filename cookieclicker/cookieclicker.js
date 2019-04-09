5fe04deac4d06eabc7e6a95090e7e91bb4511d68
TheWallovec <42898548+TheWallovec@users.noreply.github.com>
2019-04-09 20:20:02 +0200

    cookieclicker a0.3

--- cookieclicker/cookieclicker.js
+++ cookieclicker/cookieclicker.js
@@ -1,4 +1,4 @@
-var cps = 0, cpc = 1, cursorPrice = 10;
+var cps = 0, cpc = 1, cursorPrice = 10, cursorV2Price = 100;
 var cookies = document.getElementById("cookieValueDisplay").nodeValue;
 
 function gotoLogin()
@@ -27,6 +27,17 @@ document.getElementById("newCursor").addEventListener("click", function(){
     }
 })
 
+document.getElementById("newCursorV2").addEventListener("click", function(){
+    if (cookies >= cursorV2Price){
+        cps += 5;
+        cookies -= cursorV2Price;
+        cursorV2Price *= 1.5;
+        document.getElementById("cursorV2BuyPriceValue").innerHTML = Math.round(cursorV2Price);
+    } else {
+        alert("Not enough cookies!");
+    }
+})
+
 document.getElementById("cookieImage").addEventListener("click", function(){
     cookies +=cpc;
     document.getElementById("cookieValueDisplay").innerHTML = "Current cookies: " + Math.round(cookies);
@@ -34,7 +45,11 @@ document.getElementById("cookieImage").addEventListener("click", function(){
 
 setInterval(function()
 {
-    cookies += (cps/10);
+    cookies += (cps/100);
     document.getElementById("cookieValueDisplay").innerHTML = "Current cookies: " + Math.round(cookies);
     document.getElementById("cps").innerHTML = "Cookies per second: " + cps;
-}, 100);
+}, 10);
+
+function pakuj(){
+    document.getElementsByClassName("oznamy").innerHTML = " ";
+}
